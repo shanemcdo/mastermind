3e07a25778ec662481e1bfd0670c4f6a6e1c175d
Shane McDonough <shanemcdman@gmail.com>
2024-02-21 20:52:11 -0800

    get sass working

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,7 +1,9 @@
 import type { Component } from "solid-js";
+import styles from "./Game.module.scss";
 
 const Game: Component = () => {
 	return <>
+		<h1 class={styles.title}>Mastermind</h1>
 	</>;
 };
 
