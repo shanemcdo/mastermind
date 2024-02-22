366ba994e3d6cfd9a5cc40267340adb7b63c4648
Shane McDonough <shanemcdman@gmail.com>
2024-02-21 22:15:17 -0800

    add colors

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,7 +1,16 @@
 import type { Component } from "solid-js";
+import { For } from "solid-js";
+import { colors, getRandomColor } from "./colors";
 import styles from "./Game.module.scss";
 
 const Game: Component = () => {
+	const columnCount = 4;
+	const rowCount = 10;
+	const chosenColors = [];
+	for(let i = 0; i < columnCount; i++) {
+		chosenColors.push(getRandomColor());
+	}
+	console.log(chosenColors);
 	return <>
 		<h1 class={styles.title}>Mastermind</h1>
 	</>;
---
+++ src/colors.tsx
@@ -0,0 +1,16 @@
+const colorCount = 5;
+
+function calculateColors(): Array<string> {
+	const result: Array<string> = [];
+	for(let i = 0; i < colorCount ; i++) {
+		const hue = (360 / colorCount) * i;
+		result.push(`hsl(${hue}, 100%, 50%)`);
+	}
+	return result;
+}
+
+export const colors = calculateColors();
+
+export function getRandomColor(): string {
+	return colors[Math.floor(colors.length * Math.random())];
+};
